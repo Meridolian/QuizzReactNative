ff7f460b1a3cfac22663ae1bdf8f038ed8e38ef9
Mathieu Gueribout <mgueribout@gmail.com>
2019-10-25 02:26:58 +0200

    NAVIGATION DONE

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import Accueil from './components/Accueil';
-import QuizzList from './components/QuizzList';
-import Quizz from './components/Quizz';
-import Resultat from './components/Resultat';
-import Score from './components/Score';
-import Apropos from './components/Apropos';
-import { createStackNavigator } from 'react-navigation-stack';
-import BottomNavigation, { FullTab } from 'react-native-material-bottom-navigation';
-import { Ionicons } from '@expo/vector-icons';
+import React, { Component } from "react";
+import { View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import Accueil from "./components/Accueil";
+import QuizzList from "./components/QuizzList";
+import Quizz from "./components/Quizz";
+import Resultat from "./components/Resultat";
+import Score from "./components/Score";
+import Apropos from "./components/Apropos";
+import { createStackNavigator } from "react-navigation-stack";
+import BottomNavigation, { FullTab } from "react-native-material-bottom-navigation";
+import { Ionicons } from "@expo/vector-icons";
+import NavigationService from "./services/NavigationService";
+
+
+const AppNavigator = createStackNavigator(
+	{
+		Accueil: { screen: Accueil },
+		QuizzList: { screen: QuizzList },
+		Quizz: { screen: Quizz },
+		Resultat: { screen: Resultat },
+		Score: { screen: Score },
+		Apropos: { screen: Apropos }
+	},
+	{
+		defaultNavigationOptions: {
+		  header: null
+		}
+	}
+);
+
+const Router = createAppContainer(AppNavigator);
 
 export default class App extends Component {
+
+	state = {
+		activeTab: "Accueil"
+	};
 
 	tabs = [
 		{
@@ -35,14 +59,14 @@ export default class App extends Component {
 		{
 			key: "Apropos",
 			icon: "md-information-circle-outline",
-			label: "A propos",
+			label: "Apropos",
 			barColor: "#FF6F61"
-		},
-	]
+		}
+	];
 
 	renderIcon = icon => ({ isActive }) => (
 		<Ionicons size={24} color="white" name={icon} />
-	)
+	);
 
 	renderTab = ({ tab, isActive }) => (
 		<FullTab
@@ -51,37 +75,29 @@ export default class App extends Component {
 			label={tab.label}
 			renderIcon={this.renderIcon(tab.icon)}
 		/>
-	)
+	);
+
+	BottomNavigationHandler = key => {
+		this.setState({ activeTab: key });
+		NavigationService.navigate(key);
+	};
+
 	render() {
 		return (
 			<View style={{ flex: 1 }}>
-				<View style={{ flex: 1 }}>
-					<Router />
-				</View>
+				<Router
+					ref={navigatorRef => {
+					NavigationService.setTopLevelNavigator(navigatorRef);
+					}}
+					style={{ flex: 1 }}
+          		/>
 				<BottomNavigation
-					onTabPress={newTab => this.setState({ activeTab: newTab.key })}
+					activeTab={this.state.activeTab}
+					onTabPress={newTab => this.BottomNavigationHandler(newTab.key)}
 					renderTab={this.renderTab}
 					tabs={this.tabs}
 				/>
 			</View>
-		)
+		);
 	}
 }
-
-const AppNavigator = createStackNavigator(
-	{
-		Accueil: { screen: Accueil },
-		QuizzList: { screen: QuizzList },
-		Quizz: { screen: Quizz },
-		Resultat: { screen: Resultat },
-		Score: { screen: Score },
-		Apropos: { screen: Apropos },
-	},
-	{
-		initialRouteName: 'Accueil'
-	}
-);
-
-const Router = createAppContainer(AppNavigator);
-
-
